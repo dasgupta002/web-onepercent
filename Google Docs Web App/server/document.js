eac04e1de297802c9ad178f18e48a0dc47761e89
Subhrakanti Dasgupta <subhrakanti.dasgupta@gmail.com>
2021-06-18 02:40:14 +0530

    Delete Google Docs Web App directory

--- Google Docs Web App/server/document.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require('mongoose')
-
-const docSchema = new Schema({
-    _id: { type: String, required: true },
-    data: { type: Object, default: '' }
-})
-
-module.exports = model('Document', docSchema)
